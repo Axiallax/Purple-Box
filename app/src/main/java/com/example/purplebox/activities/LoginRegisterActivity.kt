8ec9617cc47f26fc2e22ed8f9d5a51da1c9942a1
ivann <ivanned235@gmail.com>
2023-10-15 21:54:59 +0800

    2.2

--- app/src/main/java/com/example/purplebox/activities/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/purplebox/activities/LoginRegisterActivity.kt
@@ -1,11 +1,12 @@
-package com.example.purplebox
+package com.example.purplebox.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.purplebox.R
 
-class MainActivity : AppCompatActivity() {
+class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_register)
     }
 }
