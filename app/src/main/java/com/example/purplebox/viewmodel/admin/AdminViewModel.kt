610bcc5184d95a3f379965295f2950226966fc85
ivann <ivanned235@gmail.com>
2023-12-23 21:47:09 +0800

    Revert "1.7 Start of AdminActivity"
    This reverts commit b9bdce8a

--- app/src/main/java/com/example/purplebox/viewmodel/admin/AdminViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.purplebox.viewmodel.admin
-
-import androidx.lifecycle.ViewModel
-import com.example.purplebox.firebaseDatabase.FirebaseDb
-
-class AdminViewModel(
-    private val firebaseDatabase: FirebaseDb
-) : ViewModel() {
-}
